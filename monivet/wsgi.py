71f8393186b27f48085cfcef6b7528293ee4ec9f
luismaster77 <luiseduardoingsis@gmail.com>
2019-09-18 20:58:22 -0500

    wsgi.py modificado para paso a producción

--- monivet/wsgi.py
+++ monivet/wsgi.py
@@ -7,31 +7,29 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os, sys
+import os,sys
 
-#path a donde esta el manage.py de nuestro proyecto Django
-from django.core.wsgi import get_wsgi_application
 
-sys.path.append(‘/home/pi/monitev/’)
+sys.path.append('/home/pi/monitev/')
 
 #referencia (en python) desde el path anterior al fichero settings.py
 #Importante hacerlo así, si hay varias instancias coriendo (en lugar de setdefault)
-os.environ[‘DJANGO_SETTINGS_MODULE’] = “monitev.settings”
+os.environ['DJANGO_SETTINGS_MODULE'] = "monitev.settings"
 #os.environ.setdefault(“DJANGO_SETTINGS_MODULE”, “proyectodjango.settings”)
 
 #prevenimos UnicodeEncodeError
-os.environ.setdefault(“LANG”, “es_CO.UTF-8”)
-os.environ.setdefault(“LC_ALL”, “es_CO.UTF-8”)
+os.environ.setdefault("LANG", "es_CO.UTF-8")
+os.environ.setdefault("LC_ALL", "es_CO.UTF-8")
 
 #activamos nuestro virtualenv
-activate_this = ‘/home/pi/monitev/env/bin/activate_this.py’
+activate_this = '/home/pi/monitev/env/bin/activate_this.py'
 execfile(activate_this, dict(__file__=activate_this))
 
 #obtenemos la aplicación
+#from django.core.wsgi import get_wsgi_application
+#path a donde esta el manage.py de nuestro proyecto Django
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-
 #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "monivet.settings")
-
 #application = get_wsgi_application()
